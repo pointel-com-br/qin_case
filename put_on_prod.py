01d5b3b185c94910d58d75229833c7d3c2493205
Éverton M. Vieira <emuvi@outlook.com.br>
2022-07-21 17:14:28 -0300

    fix the directory to publish

--- put_on_prod.py
+++ put_on_prod.py
@@ -10,4 +10,4 @@ qin_root = os.environ['QIN_ROOT']
 build()
 publish("production")
 shutil.copytree(
-    "./public", f"{qin_root}/Prod/pub/qin_desk", dirs_exist_ok=True)
+    "./public", f"{qin_root}/Prod/pub/qin_case", dirs_exist_ok=True)
